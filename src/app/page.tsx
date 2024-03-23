67e586bee2e5663e6fb019e5e258c754c27cb5e4
Pacharapat <pcp.august@gmail.com>
2024-03-23 20:53:47 +0700

    fix margin main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import LogInTextField from '@/components/TextField'
+import HotelCard from '@/components/hotelCard'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between mt-16">
     </main>
   )
 }
