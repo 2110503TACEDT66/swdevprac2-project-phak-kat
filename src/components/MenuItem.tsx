23f4e126002ab58da3c7441a86edf42a78dd62e3
pppny1920 <popo192029@gmail.com>
2024-03-23 19:55:45 +0700

    style menubar

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -4,7 +4,11 @@ export default function MenuItem({title, pageRef}:
     {title:string, pageRef:string}) {
         return (
             <Link href={pageRef}
-            className='text-center mx-5 my-auto text-white'>
+            className='text-center px-5 py-1 mx-2 my-auto
+            rounded-full
+            text-hCream text-md font-serif font-light
+            hover:bg-stone-400 hover:text-stone-600
+            active:bg-hCream'>
                 {title}
             </Link>
         );
