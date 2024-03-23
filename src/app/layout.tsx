b028a4cd384234d75659357e5381a42397b1e58a
Pacharapat <pcp.august@gmail.com>
2024-03-23 18:53:27 +0700

    fix import menubar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import MenuBar from '@/component/MenuBar'
+import MenuBar from '@/components/menubar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
