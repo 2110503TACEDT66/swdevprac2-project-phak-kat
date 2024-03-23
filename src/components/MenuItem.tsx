83265a5eaafeb4a65c0875296a4ea3cf541f0d9d
pppny1920 <popo192029@gmail.com>
2024-03-23 19:26:49 +0700

    add icon to menubar

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -4,7 +4,7 @@ export default function MenuItem({title, pageRef}:
     {title:string, pageRef:string}) {
         return (
             <Link href={pageRef}
-            className='w-1/6 text-center mx-5 text-white'>
+            className='text-center mx-5 my-auto text-white'>
                 {title}
             </Link>
         );
--- src/components/menubar.tsx
+++ src/components/menubar.tsx
@@ -1,10 +1,14 @@
 import MenuItem from "./MenuItem";
+import Image from "next/image";
 
 export default function MenuBar() {
     return (
-        <div className='w-full h-12 bg-black fixed top-0 right-0 left-0 z-50 flex flex-row justify-between'>
+        <div className='w-full h-16 bg-black fixed top-0 right-0 left-0 z-50 flex flex-row justify-between'>
             <div className='flex flex-row justify-start'>
-
+                <Image src={'/img/icon-white.svg'} 
+                alt="icon" height={0} width={0}
+                className="h-10 w-auto my-auto mx-2" 
+                />
                 <MenuItem title='Home' pageRef='/'/>
                 <MenuItem title='MyBooking' pageRef='/mybooking'/>
             </div>
