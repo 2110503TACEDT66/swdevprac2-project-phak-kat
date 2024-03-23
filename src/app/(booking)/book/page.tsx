a0fcecf2a3df27cc8a3d04292e0b5ee6a3dcacf9
pppny1920 <popo192029@gmail.com>
2024-03-24 01:09:36 +0700

    bookPage

--- src/app/(booking)/book/page.tsx
+++ src/app/(booking)/book/page.tsx
@@ -2,7 +2,11 @@ import BookingForm from '@/components/BookingForm';
 
 export default function Book() {
     return (
-      <main className="w-full flex justify-center">
+      <main className="w-full flex flex-col justify-center">
+        <h1 className='mx-auto my-12 text-3xl font-sans font-semibold
+        text-stone-900'>
+          Booking
+        </h1>
         <BookingForm/>
       </main>
     );
--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -4,7 +4,7 @@ import LogInButton from './LogInButton';
 
 export default function BookingForm() {
     return (
-        <form className='flex flex-col w-1/2 space-y-5 justify-center'>
+        <form className='flex flex-col w-1/2 space-y-5 justify-center mx-auto'>
             <div className='flex flex-row justify-between space-x-5'>
                 <HotelTextField id="name" pText='your username' lable='username' disable={true}/>
                 <HotelTextField id="hid" pText='hotel' lable='hotel' disable={true}/>
