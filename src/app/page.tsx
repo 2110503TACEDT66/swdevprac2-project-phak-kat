8619f319c4252d7465586fe487d95bf825738e47
Pacharapat <pcp.august@gmail.com>
2024-03-24 01:08:07 +0700

    fix css margintop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col mt-16">
+    <main className="flex min-h-screen flex-col">
       <div className='text-left text-xl px-10 pt-10 font-bold font-sans'>Find Hotel of Your Choice</div>
       <HotelPanel/>
     </main>
