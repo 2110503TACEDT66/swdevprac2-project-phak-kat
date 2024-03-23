99529b5b546005322f2eac116362ca1c74d030ff
Pacharapat <pcp.august@gmail.com>
2024-03-23 20:54:08 +0700

    hotel card (not finished)

--- src/components/hotelCard.tsx
+++ src/components/hotelCard.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export default function HotelCard(){
     return (
-        <div className="w-[511px] h-[417px] p-[30px] rounded-lg shadow-lg">
-            <div className='flex w-auto h-[248px] relative rounded-lg justify-center'>
+        <div className="w-[350px] h-[300px] p-[30px] rounded-lg shadow-lg">
+            <div className='flex w-auto h-[70%] relative rounded-lg justify-center'>
                 <Image src='/img/test.jpg'
                 alt='Product Picture' fill={true}
                 className='object-cover rounded-lg'/>
             </div>
-            <div className="w-full h-auto flex flex-row mt-[30px]">
-                <div className="w-[70%] flex flex-col justify-start">
+            <div className="w-full h-[30%] flex flex-row pt-6">
+                <div className="w-[80%] relative flex flex-col justify-start">
                     <p className="text-lg">Hotel name</p>
                     <p className="text-sm">Location</p>
                 </div>
-                <div className="w-[30%] flex justify-end">
-                    <button className="bg-orange-600 px-[40px] py-[10px] rounded-lg">Book</button>
+                <div className="w-[20%] flex justify-end">
+                    <button className="bg-hRed px-3 rounded-lg">Book</button>
                 </div>
             </div>
         </div>
