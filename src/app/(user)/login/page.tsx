eafdb02cb793bf1f4644b2be9e3993d71d2949e2
pppny1920 <popo192029@gmail.com>
2024-03-24 13:37:41 +0700

    Log in page

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,7 +1,41 @@
-export default function Login() {
+import GgLogInButton from "@/components/GgLogInButton";
+import HotelTextField from '@/components/HotelTextField'
+import LogInButton from "@/components/LogInButton";
+import Link from "next/link";
+
+export default function LogIn() {
     return (
-      <main className="w-full flex flex-col justify-center">
-        <h1>hi</h1>
+      <main className="w-full flex flex-col justify-center space-y-6">
+        <h1 className='mx-auto mt-12 text-3xl font-sans font-bold
+        text-stone-900'>
+          Log In
+        </h1>
+        <form className="w-full flex flex-col justify-center space-y-6">
+          <div className="w-1/2 mx-auto">
+            <div className="space-y-4">
+              <HotelTextField type='text' id="email" pText='email' lable='email' disable={false}/>
+              <HotelTextField type='password' id="password" pText='password' lable='password' disable={false}/>
+            </div>
+            <div className='text-sm font-medium text-neutral-800 font-sans
+            flex flex-row space-x-2 justify-end mt-2'>
+              <h6>Don't have an account?</h6>
+              <Link href={'/signup'} className="underline">
+                Sign Up
+              </Link>
+            </div>
+          </div>
+          <div className="mx-auto">
+            <LogInButton buttonText="log in"/>
+          </div>
+          <div className="flex flex-row justify-center space-x-4">
+            <hr className="w-48 h-0.5 my-auto bg-neutral-800 border-0 rounded"/>
+            <h6 className='text-sm font-medium text-neutral-800 font-sans'>or</h6>
+            <hr className="w-48 h-0.5 my-auto bg-neutral-800 border-0 rounded"/>
+          </div>
+          <div className="mx-auto">
+            <GgLogInButton/>
+          </div>
+        </form>
       </main>
     );
 }
