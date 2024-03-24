f8904094ecf82b5cec69af4e5aa7daa9c97a05ae
pppny1920 <popo192029@gmail.com>
2024-03-24 16:14:57 +0700

    my booking page

--- src/app/(booking)/mybooking/page.tsx
+++ src/app/(booking)/mybooking/page.tsx
@@ -5,9 +5,13 @@ export default function MyBooking() {
       <main className="w-full flex flex-col justify-center">
         <h1 className='mx-auto my-12 text-3xl font-sans font-bold
         text-stone-900'>
-          Booking
+          My Booking
         </h1>
-        <BookingCard params={'wow'}/>
+        <div className="flex flex-col space-y-6">
+            <BookingCard params={{id:'string'}}/>
+            <BookingCard params={{id:'string'}}/>
+            <BookingCard params={{id:'string'}}/>
+        </div>
       </main>
     );
   }
