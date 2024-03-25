a0cd74810ed7d6360a50e16f375b1bdffb30ff75
tatatitutatuay <thirathawat996@gmail.com>
2024-03-25 14:55:41 +0700

    add key to hotelcard

--- src/components/HotelPanel.tsx
+++ src/components/HotelPanel.tsx
@@ -4,13 +4,13 @@ import HotelCard from "./HotelCard";
 export default async function HotelPanel({hotelsJson} : {hotelsJson:HotelJson}) {
     
     const hotels = await hotelsJson
-    
+
     return (
         <div className="px-10 py-5">
             <div className="grid grid-cols-3 gap-10">
                 {
                     hotels.data.map((hotelItem:HotelItem) => (
-                        <Link href={`/${hotelItem._id}`}>
+                        <Link href={`/${hotelItem._id}`} key={hotelItem._id}>
                         <HotelCard hotelName={hotelItem.name} location={hotelItem.province}/>
                         </Link>
                     ))
