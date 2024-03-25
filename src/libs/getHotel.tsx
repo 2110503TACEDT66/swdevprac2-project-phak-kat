8c18945f06954b1d8c5a687fd2127cfe16eca268
Pacharapat <pcp.august@gmail.com>
2024-03-25 23:46:59 +0700

    edit fetching link to vercel

--- src/libs/getHotel.tsx
+++ src/libs/getHotel.tsx
@@ -1,7 +1,8 @@
 export default async function getHotel(id:string) {
-    const response = await fetch(`http://localhost:5000/api/v1/hotels/${id}`)
+    const response = await fetch(`https://hotel-backend-beta.vercel.app/api/v1/hotels/${id}`)
 
     if(!response.ok) {
+        throw new Error('Failed to fetch this hotel')
     }
 
     return await response.json()
--- src/libs/getHotels.tsx
+++ src/libs/getHotels.tsx
@@ -2,7 +2,7 @@ export default async function getHotels() {
     //add timeout
     await new Promise((resolve)=>setTimeout(resolve, 1000))
 
-    const response = await fetch('http://localhost:5000/api/v1/hotels')
+    const response = await fetch('https://hotel-backend-beta.vercel.app/api/v1/hotels', {cache:'no-store'})
     if(!response.ok) {
         throw new Error('Failed to fetch hospitals')
     }
